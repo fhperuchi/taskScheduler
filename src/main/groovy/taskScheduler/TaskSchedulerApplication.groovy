01b5b94271db5cb80013b32107db87457b49e65b
Fabio Peruchi <fabio.peruchi@tecsinapse.com.br>
2017-04-02 14:53:28 -0300

    First commit

---
+++ src/main/groovy/taskScheduler/TaskSchedulerApplication.groovy
@@ -0,0 +1,12 @@
+package taskScheduler
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TaskSchedulerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run TaskSchedulerApplication, args
+	}
+}
---
+++ src/test/groovy/taskScheduler/TaskSchedulerApplicationTests.groovy
@@ -0,0 +1,16 @@
+package taskScheduler
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class TaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
